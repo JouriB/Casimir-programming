384ce30c61597f95049e2599b6ef5341fa35e5d8
fkdevries <f.k.devries@tudelft.nl>
2016-10-26 11:35:34 +0000

    plot

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 print('Good Morning')
 r=10
-
+import matplotlib.pyplot as plt
 import test
 
 print('Radius =',r)
 print('Circumference =',test.circ(r))
 print('Surface =',test.surf(r))
+
+plt.plot([0,1,2],[3,4,5])
