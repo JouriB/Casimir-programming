eaec48b291df412398869b8a23d8dc5fcb862441
fkdevries <f.k.devries@tudelft.nl>
2016-10-27 12:01:47 +0000

    All the actions are functions now, you could run them in an jupyter notebook

--- p2arxivsearch.py
+++ p2arxivsearch.py
@@ -1,26 +1,37 @@
 import urllib
 import numpy as np
-import matplotlib as plt
-%matplotlib inline
-# Request library
-#Specify URL for search
+#import matplotlib as plt
+#%matplotlib inline
 
-#Searching for a title
-title='novel'
-maxresults='10'
-
-def readarxiv(title,maxresults=10):
-    url = 'http://export.arxiv.org/api/query?search_query=ti:'+ title + '&start=0&max_results=' + maxresults
+def readarxiv_title(title,maxresults):
+    """
+    This function searches the arxiv for a specific word in the title
+    title has to be given in the form  'title' (example: 'novel')
+    maxresults can be any integer and will limit the output number
+    data is the HTML string returned
+    """
+    url = 'http://export.arxiv.org/api/query?search_query=ti:'+ title + '&start=0&max_results=' + str(maxresults)
     data = urllib.urlopen(url).read()
     return data
 
-#print (data)
-
-# read title, abstract, authors
-
-# search for title (novel, revisit)
+def readarxiv_author(author,maxresults):
+    """
+    This function searches the arxiv for a specific author
+    author has to be given in the form  'surname_initial' (example: 'Kouwenhoven_L')
+    maxresults can be any integer and will limit the output number (although here it does not work)
+    data is the HTML string returned
+    """
+    maxresults=2
+    url = 'http://export.arxiv.org/api/query?search_query=au:'+ author + '&start=0&max_results=' + str(maxresults)
+    data = urllib.urlopen(url).read()
+    return data
 
 def finddate(data):
+    """
+    data is HTML string
+    search for specific string in every line
+    save the date found
+    """
     dates=[]
     lines=data.split('\n')
     for n in range(len(lines)):
@@ -29,31 +40,37 @@ def finddate(data):
             dates.append(lines[n][ind+11:ind+21])
     return dates
 
-dates_novel=finddate(readarxiv('novel',maxresults))
-dates_revisit=finddate(readarxiv('novel',maxresults))
-
+def abstract_word_count (data):
+    """
+    This function searches the data file for abstracts and count the number of words in it.
+    data is the HTML string
+    abswcount is a numpy array with an entry for every abstract.    
+    We could split this function up in looking for the index of a string and counting itself.
+    """
+    words=data.split() # extract the words from the query
+    sumstart=[]
+    sumend=[]
+    for i in range(len(words)-1):
+        if words[i]=="<summary>": # look up the word <summary> and save the index
+            sumstart.append(i)
+        elif words[i]=="</summary>": # look up the word </summary> and save the index
+            sumend.append(i)       
+    sumstart=np.array(sumstart) #make a numpy array for processing
+    sumend=np.array(sumend)
+    abswcount=sumend-sumstart -1 # count the number of words per abstract
+    return abswcount
+'''
+#Using the functions
+maxresults=100
+dates_novel=finddate(readarxiv_title('novel',maxresults))
+dates_revisit=finddate(readarxiv_title('novel',maxresults))
 print(dates_revisit)
-matplotlib.plot([0,1],[1,1])
 
 #Search for specific author
-author='Kouwenhoven_L'
-
-url = 'http://export.arxiv.org/api/query?search_query=au:'+ author + '&start=0&max_results=10000'
-data = urllib.urlopen(url).read()
-words=data.split() # extract the words from the query
-
-sumstart=[]
-sumend=[]
-for i in range(len(words)-1):
-    if words[i]=="<summary>": # look up the word <summary> and save the index
-        sumstart.append(i)
-    elif words[i]=="</summary>": # look up the word </summary> and save the index
-        sumend.append(i)       
-sumstart=np.array(sumstart) #make a numpy array for processing
-sumend=np.array(sumend)
-abswcount=sumend-sumstart -1 # count the number of words per abstract
+data=readarxiv_author('Kouwenhoven_L',10000)
 
 #plot the data as 
-plt.hist(abswcount)
+plt.hist(abstract_word_count(data))
 plt.xlabel("number of words in abstract")
 plt.ylabel("number of papers")
+'''
